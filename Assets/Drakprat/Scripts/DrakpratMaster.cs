39f4004983c125300f5f88ae5ab42f1d1331af04
IbbMakesGames <skoldselma0@gmail.com>
2023-02-05 00:59:02 +0100

    various saker

---
+++ Assets/Drakprat/Scripts/DrakpratMaster.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DrakpratMaster : MonoBehaviour
+{
+    private int index = 0;
+    private Button[] buttons;
+
+    public Button goodBtn;
+    public Button badBtn1;
+    public Button badBtn2;
+
+    public Sentance[] dragonSents;
+    public Sentance[] goodOptions;
+    public Sentance[] badOptions1;
+    public Sentance[] badOptions2;
+
+    [System.Serializable]
+    public class Sentance
+    {
+        public string words;
+        public int fontsize;
+    }
+}
