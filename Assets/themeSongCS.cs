df4aab8e1e1eab94db64197120ecafc6a3003e00
Tom <tom.eckerstrom@hotmail.com>
2023-02-05 17:26:12 +0100

    theme

--- Assets/themeSongCS.cs
+++ Assets/themeSongCS.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class themeSongCS : MonoBehaviour
 {
-    themeSongCS[] musicPlayer;
-
     private void Awake()
     {
-        musicPlayer = FindObjectsOfType<themeSongCS>();
-        if(musicPlayer.Length > 1)
-            Destroy(musicPlayer[1]);
+        var sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName == "Overworld" || sceneName == "MainMenu")
+            GetComponent<AudioSource>().Play();
+        else
+            GetComponent<AudioSource>().Stop();
+
 
         DontDestroyOnLoad(gameObject);
     }
