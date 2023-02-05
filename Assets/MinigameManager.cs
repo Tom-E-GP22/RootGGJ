eba3b597ef2df5dacadcded1f13f91072fa5b3ce
Tom <tom.eckerstrom@hotmail.com>
2023-02-05 13:48:27 +0100

    whack a termite score system

---
+++ Assets/MinigameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    [Header("Values")]
+    public int winScore = 25;
+    public int score;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
